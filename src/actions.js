f293c9fb15a7161d5cc8cacf26f1638af4bf06c4
Yong Zhang <zhangyong1025.zy@gmail.com>
2020-04-15 16:24:13 +0800

    Update actions

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,10 @@
 const core = require('@actions/core');
-const { Github, context } = require('@actions/github');
+const { GitHub, context } = require('@actions/github');
 
 async function run() {
 
     try {
-        const github = new Github(process.env.GITHUB_TOKEN);
+        const github = new GitHub(process.env.GITHUB_TOKEN);
         const reRunCmd = core.getInput('rerun_cmd', { required: false});
         const owner = core.getInput('repo_owner', {required: true});
         const repo = core.getInput('repo_name', {required: true});
